93b2b62ef895cbefad5470786b08c4fac9e51647
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-01 23:03:47 +0200

    Update MinimumOf3Numbers.cpp

--- MinimumOf3Numbers.cpp
+++ MinimumOf3Numbers.cpp
@@ -1,3 +1,4 @@
+//WayOne
 #include <iostream>
 using namespace std;
 int main()
@@ -16,3 +17,51 @@ int main()
 
 	return 0;
 }
+//WayTwo
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, num3;
+
+	cin >> num1 >> num2 >> num3;
+
+	if (num1 < num2) {
+		// Then either num1 or num3 is the answer
+		if (num1 < num3)
+			cout << num1 << "\n";
+		else
+			cout << num3 << "\n";
+	} else	// Then either num2 or num3 is the answer
+	{
+		if (num2 < num3)
+			cout << num2 << "\n";
+		else
+			cout << num3 << "\n";
+	}
+
+	return 0;
+}
+
+//WayThree
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, num3;
+
+	cin >> num1 >> num2 >> num3;
+
+	int answer = num1;
+
+	if (answer > num2)
+		answer = num2;
+
+	if (answer > num3)
+		answer = num3;
+
+	cout << answer << "\n";
+
+	return 0;
+}
+
